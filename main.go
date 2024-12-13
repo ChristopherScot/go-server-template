9c499daca4781838b30a2bb3c504d9fc401ebb39
ChristopherScot <16506996+ChristopherScot@users.noreply.github.com>
2024-12-12 22:41:07 -0500

    some more nonsense logging

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome team"))
+		logger.ErrorContext(r.Context(), "This isn't a real error",
+			"key1", "value1",
+			"key2", "value2",
+		)
 	})
 	slog.Info("Server started at :3000")
 	http.ListenAndServe("0.0.0.0:3000", r)
